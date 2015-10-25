804c67252e63aaf622d21e5d2fed5ec20fce065f
Pavan <pavan.malladi@gmail.com>
2015-10-25 17:28:02 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,9 +1,10 @@
 library(maps)
 library(mapdata)
 
+fileName <- "Countries_And_Population.csv"
+popData <- read.csv2(fileName, sep = ",")
+
 shinyServer(function(input, output) {
-  fileName <- "Countries_And_Population.csv"
-  popData <- read.csv2(fileName, sep = ",")
   output$map <- renderPlot({map('worldHires',popData$Country[input$populationRank])})
   output$country <- renderText({paste(popData$Country[input$populationRank])})
 })
